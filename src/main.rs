d44fa1fbdd97c35c1ba1656abab2cccc752b8122
whn <1874695345@qq.com>
2024-10-07 16:52:10 +0800

    [send correlation id]

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{net::TcpListener, io::Write};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
+                let buf: [u8; 8] = [0, 0, 0, 0, 0, 0, 0, 7];
+                stream.write(&buf).unwrap();
                 println!("accepted new connection");
             }
             Err(e) => {
