4646224b44c46af5ee4da80ab02e92d44aed4011
whn <1874695345@qq.com>
2024-10-08 09:52:30 +0800

    [parse correlation id]

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
 #![allow(unused_imports)]
-use std::{net::TcpListener, io::Write};
+use std::{io::{Cursor, Read, Seek, Write}, net::TcpListener};
 
-fn main() {
+use tokio::io::AsyncReadExt;
+
+#[tokio::main]
+async fn main() {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                let buf: [u8; 8] = [0, 0, 0, 0, 0, 0, 0, 7];
-                stream.write(&buf).unwrap();
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+
+                let mut buf = [0_u8; 4];
+                _stream.read_exact(buf.as_mut_slice()).unwrap();
+                let mut render = Cursor::new(buf);
+                let len = render.read_u32().await.unwrap();
+
+                let mut request = vec![0u8; len.try_into().unwrap()];
+                _stream.read(request.as_mut_slice()).unwrap();
+
+                let mut render = Cursor::new(request);
+                render.read_u16().await.unwrap(); // request_api_key
+                render.read_u16().await.unwrap(); // request_api_version
+                let correlation_id = render.read_u32().await.unwrap();
+                println!("correlation id: {}", correlation_id);
+                _stream.write_all([0, 0, 0, 4].as_slice()).unwrap();
+                _stream.write_all(&correlation_id.to_be_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
